0e7c5a1cde8e32af4584fdd4d55c9c4366f0c425
Kees Postma <kk.postma@gmail.com>
2017-07-19 16:46:49 +0100

    Adds School model through Devise with all associated views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   # Devise authentication for users
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
+  # Devise authentication for schools
+  devise_for :schools, controllers: { sessions: 'schools/sessions' }
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
