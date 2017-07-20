2b220c4079bc3ad02cc2b7439450f269cc33626b
Kees Postma <kk.postma@gmail.com>
2017-07-20 17:23:32 +0100

    Accomodates for two user types in Navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   # Skip Devise authentication for certain pages
   skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_school_admin!, only: :home
 
   def home
   end
+
+
+
 end
