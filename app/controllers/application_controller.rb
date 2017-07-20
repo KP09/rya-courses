b43999299ef8a8ea9630c01fefaf60e3fe943c92
Kees Postma <kk.postma@gmail.com>
2017-07-20 16:11:59 +0100

    Removes authentications from Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,6 @@ class ApplicationController < ActionController::Base
 
   # White-list approach to authentication
   before_action :authenticate_user!
-
-  # Add permitted fields to User sign in
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # Method that configures the parameters permitted by Devise
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-
-    # For additional fields in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  end
+  before_action :authenticate_school_admin!
 
 end
